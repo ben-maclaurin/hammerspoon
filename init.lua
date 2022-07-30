d2ccecf2dbeb3fbbf0c7f231d5ef63beed02ccbb
Ben <54992484+ben-maclaurin@users.noreply.github.com>
2022-07-30 15:42:55 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -46,7 +46,7 @@ hs.hotkey.bind({"cmd", "shift"}, "J", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "shift"}, "F", function()
+hs.hotkey.bind({"cmd", "shift"}, "Return", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -59,65 +59,24 @@ hs.hotkey.bind({"cmd", "shift"}, "F", function()
   win:setFrame(f)
 end)
 
--- Standard apps
+-- Middle mouse button  
 
-hs.hotkey.bind({"cmd", "shift"}, "pad7", function()
-  hs.application.open("Firefox.app")
-end)
+overrideOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+    local mouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
 
-hs.hotkey.bind({"cmd", "shift"}, "pad8", function()
-  hs.application.open("Emacs.app")
-end)
+    if mouseButton == 2 then
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
 
-hs.hotkey.bind({"cmd", "shift"}, "pad9", function()
-  hs.application.open("Finder.app")
-end)
-
--- Custom apps
-
-hs.hotkey.bind({"cmd", "shift"}, "pad4", function()
-  hs.application.open("Sublime Text.app")
-end)
-
-hs.hotkey.bind({"cmd", "shift"}, "pad5", function()
-  hs.application.open("Alacritty.app")
-end)
-
-hs.hotkey.bind({"cmd", "shift"}, "pad6", function()
-  hs.application.open("Spotify.app")
-end)
-
--- Browser actions
-
-hs.hotkey.bind({"cmd", "shift"}, "pad1", function()
-  hs.application.open("Firefox.app")
-  hs.eventtap.event.newKeyEvent({"cmd"}, "t", true):post()
-end)
-
-hs.hotkey.bind({"cmd", "shift"}, "pad2", function()
-  hs.application.open("Firefox.app")
-  hs.eventtap.event.newKeyEvent({"cmd"}, "y", true):post()
-end)
-
-hs.hotkey.bind({"cmd", "shift"}, "pad3", function()
-  hs.application.open("Firefox.app")
-  hs.eventtap.event.newKeyEvent({"cmd"}, "l", true):post()
-  hs.eventtap.keyStrokes("http://localhost:3000/")
-  hs.eventtap.event.newKeyEvent(nil, "return", true):post()
-end)
-
--- Hammerspoon
-
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
+      f.x = max.x 
+      f.y = max.y
+      f.w = max.w
+      f.h = max.h 
+      win:setFrame(f)
     end
-    if doReload then
-        hs.reload()
-    end
-end
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.notify.new({title="Hammerspoon", informativeText="Config reloaded"}):send()
+
+end)
+
+overrideOtherMouseDown:start()
